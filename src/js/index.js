5598385b3c702bfaa0eda0bfa87111bec4c2d56c
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-11 01:24:41 +0000

    Adding my infos

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
-import resume from "../assets/resume.pdf";
+import resume from "../assets/Alassane-resume.pdf";
 
 export default function initApp() {
   initSr();
