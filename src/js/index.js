710c29d5bb9aeded864f74c975467e9cbd2dc954
Alassane Ndoye <aloutndoye.an@gmail.com>
2023-02-13 13:07:23 +0000

    add updated resume

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
-import resume from "../assets/Alassane-Resume.pdf";
+import resume from "../assets/alassane-ndoye-resume.pdf";
 
 export default function initApp() {
   initSr();
